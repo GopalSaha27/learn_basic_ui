a1c32e2d938c2e54577f5e8125fd6f35a84d4696
GopalSaha21 <2220216@iub.edu.bd>
2025-06-07 13:24:47 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Basic Ui",
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  Home({super.key});
+
+  List<String> friendlist = [
+    'Nafi',
+    'Gopal',
+    'ABM Mozahid',
+    'Dipu Das',
+    'Noorjahan'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF002B5B),
+        centerTitle: true,
+        title: Text(
+          "Basic Ui",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.yellow,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.edit,
+              color: Colors.yellow,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.delete,
+              color: Colors.yellow,
+            ),
+          ),
+        ],
+        // leading: Icon(
+        //   Icons.add,
+        //   color: Colors.yellow,
+        // ),
+      ),
+      drawer: Drawer(
+        shadowColor: Colors.red,
+        backgroundColor: Colors.white54,
+        width: 250,
+        child: Column(
+          children: [
+            Text(
+              "Hello Programmer...",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(0xFF002B5B),
+        unselectedItemColor: Colors.green,
+        currentIndex: 0,
+        selectedItemColor: Colors.yellow,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+        ],
+      ),
+    );
+  }
+}
